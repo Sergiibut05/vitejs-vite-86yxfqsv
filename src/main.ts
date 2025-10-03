2864e25ae973e7e031751d7fb9b7adf3001c3ae6
Sergii Butrii <sergiibutrii555@gmail.com>
2025-10-03 14:42:28 +0200

    goku

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,33 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+async function fetchCharacter(): Promise<any> {
+  const res = await fetch('https://dragonball-api.com/api/characters');
+  if (!res.ok) {
+    throw new Error('No se pudo cargar detalles');
+  }
+  return await res.json();
+}
+
+async function renderCharacter() {
+  try {
+    const characters = await fetchCharacter();
+    let innerhtml = ``
+    for(let i=0; i<characters.meta.itemCount; i++){
+      innerhtml = innerhtml+`
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="${characters.items[i].image}" class="logo" alt="Vite logo" />
+        </a>
+        <h1>${characters.items[i].name}</h1>
+      </div>
+    `;
+    }
+    document.querySelector<HTMLDivElement>('#app')!.innerHTML = innerhtml;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+renderCharacter();
